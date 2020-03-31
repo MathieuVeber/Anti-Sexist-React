092a517ba3c44b7ca69bb9f3630d49109858593b
MathieuVeber <43341681+MathieuVeber@users.noreply.github.com>
2020-04-01 01:39:43 +0200

    Validation ok except for labels

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -2,6 +2,10 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
+// Form validation
+import { Formik } from 'formik'
+import * as yup from 'yup'
+
 // Actions
 import { login } from '../actions/userAction'
 
@@ -15,17 +19,9 @@ import Card from 'react-bootstrap/Card'
 
 
 export class UserLogin extends Component {
-    constructor(props) {
-        super(props);
-        this.user = React.createRef();
-        this.password = React.createRef();
-        //this.isValid = React.createRef();
-    }
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-        event.stopPropagation();
-        this.props.login(this.user.current.value,this.password.current.value);
+    handleSubmit = (user,password) => {
+        this.props.login(user,password);
     }
 
     render() {
@@ -34,6 +30,20 @@ export class UserLogin extends Component {
         if(this.props.loggedIn) {
             return <Redirect to="/"/>
         }
+
+        const user = this.props.user ? this.props.user : "" ;
+        const password = this.props.password ? this.props.password : "" ;
+
+        const formSchema = yup.object({
+            user: yup.string()
+                .min(1,"Un email ou un pseudo est requis")
+                .max(64, "Le champ ne dépasse pas 64 caractères")
+                .required("Un email ou un pseudo est requis"),
+            password: yup.string()
+                .min(6,"Votre mot de passe n'est pas assez long")
+                .max(64,"Votre mot de passe est trop long")
+                .required("Difficile de se connecter sans mot de passe"),
+        });
 
         return (
             <div className="login">
@@ -44,23 +54,58 @@ export class UserLogin extends Component {
                                 Connexion
                             </Card.Header>
                             <Card.Body className="p-4">
-                                <Form onSubmit={this.handleSubmit}> {/*validated={this.isValid} noValidate >*/}
+                            <Formik
+                                validationSchema={formSchema}
+                                onSubmit={(values) => this.handleSubmit(values.user,values.password)}
+                                initialValues={{
+                                    user: user,
+                                    password: password,
+                            }}>
+                            {({
+                                handleSubmit,
+                                handleChange,
+                                handleBlur,
+                                values,
+                                touched,
+                                isValid,
+                                errors,
+                            }) => (
+                                <Form noValidate onSubmit={handleSubmit}>
+                                    <Form.Group controlId="validationFormik01">
+                                        <Form.Label>Identifiant</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            placeholder="Email ou pseudo"
+                                            name="user"
+                                            value={values.user}
+                                            onChange={handleChange}
+                                            isValid={touched.user && !errors.user}
+                                            isInvalid={!!errors.user}
+                                        />
+                                        <Form.Control.Feedback type="valid" >Parfait !</Form.Control.Feedback>
+                                        <Form.Control.Feedback type="invalid" >{errors.user}</Form.Control.Feedback>
+                                    </Form.Group>
                                     
-                                    <Form.Group controlId="user">
-                                        <Card.Title><Form.Label>Identifiant</Form.Label></Card.Title>
-                                        <Form.Control type="text" placeholder="Email ou pseudo" ref={this.user} required />
-                                        <Form.Control.Feedback type="invalid">Pas facile de vous identifier comme ça...</Form.Control.Feedback>
+                                    <Form.Group controlId="validationFormik02">
+                                        <Form.Label>Mot de passe</Form.Label>
+                                        <Form.Control
+                                            type="password"
+                                            placeholder="******"
+                                            name="password"
+                                            value={values.password}
+                                            onChange={handleChange}
+                                            isValid={touched.password && !errors.password}
+                                            isInvalid={!!errors.password}
+                                        />
+                                        <Form.Control.Feedback type="valid" >Parfait !</Form.Control.Feedback>
+                                        <Form.Control.Feedback type="invalid" >{errors.password}</Form.Control.Feedback>
                                     </Form.Group>
-
-                                    <Form.Group controlId="password">
-                                    <Card.Title><Form.Label>Mot de passe</Form.Label></Card.Title>
-                                        <Form.Control type="password" placeholder="Au moins 6 caractères" ref={this.password} required />
-                                        <Form.Control.Feedback type="invalid">Pas facile de vous identifier comme ça...</Form.Control.Feedback>
-                                    </Form.Group>
-                                    <Button variant="info" type="submit">
+                                    <Button variant="outline-light" type="submit">
                                         Se connecter
                                     </Button>
                                 </Form>
+                                )}
+                                </Formik>
                             </Card.Body>
                         </Card>
                     </Row>
